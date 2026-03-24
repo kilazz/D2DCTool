fb59d67cdf6b925d85eb5c660fc9564661442a8b
Your kilazz <zed2000091@hotmail.com>
2026-03-24 05:18:01 +0300

    Rename tags to hash dictionaries and update Program.cs

--- D2DCTool/Program.cs
+++ D2DCTool/Program.cs
@@ -8,9 +8,9 @@ class Program
     [STAThread]
     public static void Main(string[] args)
     {
-        if (System.IO.File.Exists("tags.txt"))
+        if (System.IO.File.Exists("hash_dictionary.txt"))
         {
-            XmlConverter.LoadHashes(System.IO.File.ReadAllLines("tags.txt"));
+            XmlConverter.LoadHashes(System.IO.File.ReadAllLines("hash_dictionary.txt"));
         }
 
         BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
